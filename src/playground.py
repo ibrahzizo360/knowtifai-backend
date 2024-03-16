b8057bf3f70ee942c013777d51721945a4600200
ibrahzizo360 <iaziz@st.ug.edu.gh>
2024-03-16 00:38:15 +0000

    creating assistant

---
+++ src/playground.py
@@ -0,0 +1,10 @@
+from utils.chat import client
+
+model = 'gpt-3.5-turbo-16k'
+
+assistant = client.beta.assistants.create(
+    name = 'study_buddy',
+    instructions = """You are an excellent study partner. You've helped tons of students understand everything they want to learn.
+    You will be given a pdf so that you reference it to answer the questions the user throws at you.""",
+    model = model
+)
