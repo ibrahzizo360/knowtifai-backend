4daa3caca4260e78a93ab2353d60ecebb62aa499
ibrahzizo360 <iaziz@st.ug.edu.gh>
2024-03-17 13:08:49 +0000

    doing more stuffs

--- src/playground.py
+++ src/playground.py
@@ -2,33 +2,63 @@ from utils.chat import client
 
 model = 'gpt-3.5-turbo-16k'
 
-assistant = client.beta.assistants.create(
-    name = 'study_buddy',
-    instructions = """You are an excellent study partner. You've helped tons of students understand everything they want to learn.
-    You will be given a pdf so that you reference it to answer the questions the user throws at you.""",
-    model = model
-)
+# assistant = client.beta.assistants.create(
+#     name = 'study_buddy',
+#     instructions = """You are an excellent study partner. You've helped tons of students understand everything they want to learn.
+#     You will be given a pdf so that you reference it to answer the questions the user throws at you.""",
+#     model = model
+# )
 
-assistant_id = assistant.id
-print(assistant_id)
+# assistant_id = assistant.id
+# print(assistant_id)
 
-thread = client.beta.threads.create(
-    messages = [
-        {
-            "role": "user",
-            "content": "Who are you?"
-        }
-    ]
-)
+# thread = client.beta.threads.create(
+#     messages = [
+#         {
+#             "role": "user",
+#             "content": "Who are you?"
+#         }
+#     ]
+# )
 
-thread_id = thread.id
+# thread_id = thread.id
 
-print(thread_id)
+# print(thread_id)
 
 
-class AssitantManager:
+class AssistantManager:
     thread_id: None
     assistant_id: None
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, model: str = model, ) -> None:
+        self.client = client
+        self.assistant = None
+        self.thread = None
+        self.run = None
+        self.answer = None
+
+        if AssistantManager.assistant_id:
+            self.assistant = self.client.beta.assistants.retrieve(
+                assistant_id = AssistantManager.assistant_id
+            )
+
+        if AssistantManager.thread_id:
+            self.thread = self.client.beta.thread.retrieve(
+                thread_id = AssistantManager.thread_id
+            )
+
+    def create_assistant(self, name, instructions, tools):
+        if not self.assistant:
+            self.assistant = self.client.beta.assistants.create(
+                name = name,
+                instructions = instructions,
+                tools = tools
+            )
+            AssistantManager.assistant_id = self.assistant.id
+            print(f"AsistantID::: {self.assistant.id}")       
+        
+    def create_thread(self):
+        if not AssistantManager.thread_id:
+            self.thread = self.client.beta.threads.create(
+                
+            )
